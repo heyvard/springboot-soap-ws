7b7d036321241d5ffd5bb2f35c4d2dd343398b10
Håvard <havard.stigen.andersen@gmail.com>
2016-06-26 00:35:39 +0200

    Enkel ws med spring boot

--- code/src/main/java/no/havard/endpoint/SimpleServiceEndpoint.java
+++ code/src/main/java/no/havard/endpoint/SimpleServiceEndpoint.java
@@ -5,6 +5,7 @@ import no.havard.header.Header;
 import no.havard.service.SimpleService;
 
 import javax.xml.bind.annotation.XmlElement;
+import java.time.Instant;
 
 
 public class SimpleServiceEndpoint implements SimpleService {
@@ -12,7 +13,7 @@ public class SimpleServiceEndpoint implements SimpleService {
 
     @Override
     public String ping(@XmlElement(required = true) String input, @XmlElement(required = true) Header header) {
-        return "hello " + header.getBrukerId() + " " + header.getTidspunkt();
+        return "hello " + header.getBrukerId() + " " + Instant.now();
 
     }
 }
